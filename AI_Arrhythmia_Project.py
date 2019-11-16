0da4a1b76ed35bdbf5102e4372dea35d51e9dff7
Matthew Kaminski <kaminskimateusz7@gmail.com>
2019-11-15 20:49:36 -0500

    95% accuracy with neural net???

--- AI_Arrhythmia_Project.py
+++ AI_Arrhythmia_Project.py
@@ -55,12 +55,12 @@ X= np.array(X).T
 #Get a test and test tests at a 10/90 split. 
 X_train, X_test, Y_train, Y_test = train_test_split(X, labels, test_size=0.6, random_state=0)
 
-rfc_classifer = RandomForestClassifier(n_estimators=15, max_depth=16,random_state=0).fit(X_train, Y_train)
+# rfc_classifer = RandomForestClassifier(n_estimators=15, max_depth=16,random_state=0).fit(X_train, Y_train)
 
 #agglo_classifer = AgglomerativeClustering(n_clusters=16).fit(X_train)
 model = Sequential()
 model.add(Dense(units=32, activation='relu', input_dim=9))
-model.add(Dense(units=16, activation='softmax'))
+model.add(Dense(units=17, activation='softmax'))
 model.compile(loss='categorical_crossentropy',
               optimizer='sgd',
               metrics=['accuracy'])
@@ -69,15 +69,15 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
 
 #Create a one-hot array of answer keys
-Y = class_to_onehot(Y_train)
+one_hot_labels = keras.utils.to_categorical(Y_train, num_classes=17)
 
 # x_train and y_train are Numpy arrays --just like in the Scikit-Learn API.
-model.fit(X_train, Y, epochs=10000,verbose=0)
+model.fit(X_train, one_hot_labels, epochs=10000,verbose=0)
 #loss_and_metrics = model.evaluate(X_train, Y, batch_size=128)
 classes = model.predict(X_train, batch_size=128)
 
 #Decode onehot array
-hypo = onehot_to_class(np.round(classes))
+hypo = np.argmax(classes, axis=1)
 
 #hypo0 = rfc_classifer.predict(X_train)
 #score0 = f1_score(Y_train, hypo0, average='macro')
