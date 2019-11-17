121f8af9fe2c0bdb5a2ac4e99439b9f755763492
Pete <psubacz@gmail.com>
2019-11-16 21:42:54 -0500

    Created feature generator and class pruning

--- AI_Arrhythmia_Project.py
+++ AI_Arrhythmia_Project.py
@@ -7,7 +7,8 @@ Created on Fri Oct 18 19:30:30 2019
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
-#import data_ingestion as di
+import data_ingestion as di
+import copy
 #Scikit-Learn Modules
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cluster import AgglomerativeClustering
@@ -17,7 +18,36 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 
 #Classification Methods
-from neural_network import run_NN
+#from neural_network import run_NN
+
+
+def reclass_data(labels,reclass_to):
+    if reclass_to == 4:
+        for i in range(0, labels.shape[0]):
+            if (labels[i]==5 or labels[i]==7 or labels[i]==8 or labels[i]==9 or labels[i]==11 or labels[i]==12 or labels[i]==13 or labels[i]==14 or labels[i]==15 or labels[i]==3 or labels[i]==4 or labels[i]==6):
+                 labels[i] = 16
+        for i in range(0, labels.shape[0]):
+            if(labels[i]==10):
+                labels[i] = 3
+            if(labels[i]==16):
+                labels[i] = 4
+    elif reclass_to == 2:
+        for i in range(0, labels.shape[0]):
+            if (labels[i]>=2):
+                 labels[i] = 2
+    return labels
+
+def generate_features(features):
+    n_features=copy.deepcopy(features)
+    z =np.concatenate((features,n_features**2),axis =1)
+    print(z.shape)
+    z =np.concatenate((z,n_features**3),axis =1)
+    print(z.shape)
+    z =np.concatenate((z,np.sqrt(n_features)),axis =1)
+    print(z.shape)
+    z =np.concatenate((z,np.cbrt(n_features)),axis =1)
+    print(z.shape)
+    return z
 
 #Parse a data file and return the features and labels as a numpy ndarray
 dataset, labels = di.parse_data_file('arrhythmia.csv') 
@@ -26,13 +56,72 @@ dataset = dataset[['Age'
                  , 'Weight'
                  , 'Average QRS Duration'
                  , 'Average P-R interval'
+                 , 'Average Q-T interval'
                  , 'Average P interval'
+                 , 'Average T interval'
+                 , 'T angle'
+                 , 'P angle'
+                 , 'QRST angle'
+                 , 'J angle'
                  , 'QRS angle'
                  , 'Heart rate'
+                 , 'V1: Amplitude of Q wave'
+                 , 'V1: Amplitude of R wave'	
+                 , 'V1: Amplitude of S wave' 
+                 , 'V1: Amplitude of P wave'
+                 , 'V1: Amplitude of T wave'
+                 , 'V1: QRSA'
+                 , 'V1: QRSTA'
+                 , 'V2: Amplitude of Q wave'
+                 , 'V2: Amplitude of R wave'	
+                 , 'V2: Amplitude of S wave' 
+                 , 'V2: Amplitude of P wave'
+                 , 'V2: Amplitude of T wave'
+                 , 'V2: QRSA'
+                 , 'V2: QRSTA'
+                 , 'V3: Amplitude of Q wave'
+                 , 'V3: Amplitude of R wave'	
+                 , 'V3: Amplitude of S wave' 
+                 , 'V3: Amplitude of P wave'
+                 , 'V3: Amplitude of T wave'
+                 , 'V3: QRSA'
+                 , 'V3: QRSTA'
+                 , 'V4: Amplitude of Q wave'
+                 , 'V4: Amplitude of R wave'	
+                 , 'V4: Amplitude of S wave' 
+                 , 'V4: Amplitude of P wave'
+                 , 'V4: Amplitude of T wave'
+                 , 'V4: QRSA'
+                 , 'V4: QRSTA'
+                 , 'V5: Amplitude of Q wave'
+                 , 'V5: Amplitude of R wave'	
+                 , 'V5: Amplitude of S wave' 
+                 , 'V5: Amplitude of P wave'
+                 , 'V5: Amplitude of T wave'
+                 , 'V5: QRSA'
+                 , 'V5: QRSTA'
+                 , 'V5: Amplitude of Q wave'
+                 , 'V5: Amplitude of R wave'	
+                 , 'V5: Amplitude of S wave' 
+                 , 'V5: Amplitude of P wave'
+                 , 'V5: Amplitude of T wave'
+                 , 'V5: QRSA'
+                 , 'V5: QRSTA'
+                 , 'V6: Amplitude of Q wave'
+                 , 'V6: Amplitude of R wave'	
+                 , 'V6: Amplitude of S wave' 
+                 , 'V6: Amplitude of P wave'
+                 , 'V6: Amplitude of T wave'
+                 , 'V6: QRSA'
+                 , 'V6: QRSTA'
                  ]]
 
 features = dataset.to_numpy()
 labels = labels.to_numpy()
+
+#Generate new 'hidden features'
+features = generate_features(features)
+labels = reclass_data(labels,4)
 
 X = np.zeros((len(dataset.columns),features.shape[0]))
 for i in np.arange(0,len(dataset.columns)):
@@ -42,14 +131,23 @@ X= np.array(X).T
 #Get a test and test tests at a 10/90 split. 
 X_train, X_test, Y_train, Y_test = train_test_split(X, labels, test_size=0.1, random_state=0)
 
-run_NN(X_train, X_test, Y_train, Y_test,dataset)
-# rfc_classifer = RandomForestClassifier(n_estimators=15, max_depth=16,random_state=0).fit(X_train, Y_train)
+#Neural Network Dataset
+#run_NN(X_train, X_test, Y_train, Y_test,dataset)
 
-#agglo_classifer = AgglomerativeClustering(n_clusters=16).fit(X_train)
 
-#hypo0 = rfc_classifer.predict(X_train)
-#score0 = f1_score(Y_train, hypo0, average='macro')
-
+#Random Forest Classifier
+rfc_classifer = RandomForestClassifier(n_estimators=140120, max_depth=100).fit(X_train, Y_train)
+hypo = rfc_classifer.predict(X_train)
+score = f1_score(Y_train, hypo, average='macro')
+print('F1_Measure: %F' % (score))
+hypo = rfc_classifer.predict(X_test)
 score = f1_score(Y_test, hypo, average='macro')
 print('F1_Measure: %F' % (score))
+
+#Naive Bayes Classifier
+
+
+
+#score = f1_score(Y_test, hypo, average='macro')
+
 #print('F1_Measure: %F' % (score1))
--- test.py
+++ test.py
@@ -1,50 +1,150 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov  6 21:01:31 2019
+Created on Sat Nov 16 21:15:45 2019
 
 @author: Eastwind
 """
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 19:30:30 2019
+
+@author: Eastwind
+"""
+import matplotlib.pyplot as plt
 import numpy as np
-##Keras Modules
-#import keras
-#from keras.models import Sequential
-#from keras.layers import Dense, Activation
+import random as rd
+import data_ingestion as di
+import copy
+#Scikit-Learn Modules
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.svm import SVC
+from sklearn.datasets import make_classification
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+
+#Classification Methods
+#from neural_network import run_NN
 
 
-#def class_to_onehot(class_label):
-#    Y = np.zeros((class_label.shape[0],16))
-#    for i in range(0,class_label.shape[0]):
-#        Y[i][int(class_label[i])-1] = 1
-#    return Y
-#
-#def onehot_to_class(onehot_array):
-#    H = np.zeros((onehot_array.shape[0],))
-#    for i in range(0,onehot_array.shape[0]):
-#        #Get the index of the one hot array and convert to singleton list
-#        H[i] = np.where(onehot_array[i] == 1)[0]
-#    return H
+def reclass_data(labels,reclass_to):
+    if reclass_to == 4:
+        for i in range(0, labels.shape[0]):
+            if (labels[i]==5 or labels[i]==7 or labels[i]==8 or labels[i]==9 or labels[i]==11 or labels[i]==12 or labels[i]==13 or labels[i]==14 or labels[i]==15 or labels[i]==3 or labels[i]==4 or labels[i]==6):
+                 labels[i] = 16
+        for i in range(0, labels.shape[0]):
+            if(labels[i]==10):
+                labels[i] = 3
+            if(labels[i]==16):
+                labels[i] = 4
+    elif reclass_to == 2:
+        for i in range(0, labels.shape[0]):
+            if (labels[i]>=2):
+                 labels[i] = 2
+    return labels
 
-#
-##agglo_classifer = AgglomerativeClustering(n_clusters=16).fit(X_train)
-#model = Sequential()
-#model.add(Dense(units=32, activation='relu', input_dim=9))
-#model.add(Dense(units=16, activation='softmax'))
-#model.compile(loss='categorical_crossentropy',
-#              optimizer='sgd',
-#              metrics=['accuracy'])
-#
-#model.compile(loss=keras.losses.categorical_crossentropy,
-#              optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
-#
-##Create a one-hot array of answer keys
-#Y = class_to_onehot(Y_train)
-#
-## x_train and y_train are Nusmpy arrays --just like in the Scikit-Learn API.
-#model.fit(X_train, Y, epochs=10000,verbose=1)
-##loss_and_metrics = model.evaluate(X_train, Y, batch_size=128)
-#classes = model.predict(X_train, batch_size=128)
-#
-##Decode onehot array
-#hypo1 = onehot_to_class(np.round(classes))
-#score1 = f1_score(Y_train, hypo1, average='macro')
-#print('F1_Measure: %F' % (score1))
+def generate_features(features):
+    n_features=copy.deepcopy(features)
+    z =np.concatenate((features,n_features**2),axis =1)
+    print(z.shape)
+    z =np.concatenate((z,n_features**3),axis =1)
+    print(z.shape)
+    z =np.concatenate((z,np.sqrt(n_features)),axis =1)
+    print(z.shape)
+    z =np.concatenate((z,np.cbrt(n_features)),axis =1)
+    print(z.shape)
+    return z
+
+#Parse a data file and return the features and labels as a numpy ndarray
+dataset, labels = di.parse_data_file('arrhythmia.csv') 
+
+dataset = dataset[['Age'
+                 , 'Weight'
+                 , 'Average QRS Duration'
+                 , 'Average P-R interval'
+                 , 'Average Q-T interval'
+                 , 'Average P interval'
+                 , 'Average T interval'
+                 , 'T angle'
+                 , 'P angle'
+                 , 'QRST angle'
+                 , 'J angle'
+                 , 'QRS angle'
+                 , 'Heart rate'
+                 , 'V1: Amplitude of Q wave'
+                 , 'V1: Amplitude of R wave'	
+                 , 'V1: Amplitude of S wave' 
+                 , 'V1: Amplitude of P wave'
+                 , 'V1: Amplitude of T wave'
+                 , 'V1: QRSA'
+                 , 'V1: QRSTA'
+                 , 'V2: Amplitude of Q wave'
+                 , 'V2: Amplitude of R wave'	
+                 , 'V2: Amplitude of S wave' 
+                 , 'V2: Amplitude of P wave'
+                 , 'V2: Amplitude of T wave'
+                 , 'V2: QRSA'
+                 , 'V2: QRSTA'
+                 , 'V3: Amplitude of Q wave'
+                 , 'V3: Amplitude of R wave'	
+                 , 'V3: Amplitude of S wave' 
+                 , 'V3: Amplitude of P wave'
+                 , 'V3: Amplitude of T wave'
+                 , 'V3: QRSA'
+                 , 'V3: QRSTA'
+                 , 'V4: Amplitude of Q wave'
+                 , 'V4: Amplitude of R wave'	
+                 , 'V4: Amplitude of S wave' 
+                 , 'V4: Amplitude of P wave'
+                 , 'V4: Amplitude of T wave'
+                 , 'V4: QRSA'
+                 , 'V4: QRSTA'
+                 , 'V5: Amplitude of Q wave'
+                 , 'V5: Amplitude of R wave'	
+                 , 'V5: Amplitude of S wave' 
+                 , 'V5: Amplitude of P wave'
+                 , 'V5: Amplitude of T wave'
+                 , 'V5: QRSA'
+                 , 'V5: QRSTA'
+                 , 'V5: Amplitude of Q wave'
+                 , 'V5: Amplitude of R wave'	
+                 , 'V5: Amplitude of S wave' 
+                 , 'V5: Amplitude of P wave'
+                 , 'V5: Amplitude of T wave'
+                 , 'V5: QRSA'
+                 , 'V5: QRSTA'
+                 , 'V6: Amplitude of Q wave'
+                 , 'V6: Amplitude of R wave'	
+                 , 'V6: Amplitude of S wave' 
+                 , 'V6: Amplitude of P wave'
+                 , 'V6: Amplitude of T wave'
+                 , 'V6: QRSA'
+                 , 'V6: QRSTA'
+                 ]]
+
+features = dataset.to_numpy()
+labels = labels.to_numpy()
+
+#Generate new 'hidden features'
+features = generate_features(features)
+labels = reclass_data(labels,4)
+
+X = np.zeros((len(dataset.columns),features.shape[0]))
+for i in np.arange(0,len(dataset.columns)):
+    X[i] = features.T[i]
+X= np.array(X).T
+
+#Get a test and test tests at a 10/90 split. 
+X_train, X_test, Y_train, Y_test = train_test_split(X, labels, test_size=0.1, random_state=0)
+
+#Support Vector Machine
+clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
+clf.fit(X_train, Y_train)  
+hypo = clf.predict(X_train)
+score = f1_score(Y_train, hypo, average='macro')
+print('F1_Measure: %F' % (score))
+hypo = clf.predict(X_test)
+score = f1_score(Y_test, hypo, average='macro')
+print('F1_Measure: %F' % (score))
